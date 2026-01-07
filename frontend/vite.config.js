5673eb7a0c87c891402cad898c802e0f354b088d
aayberk7 <ayberkk7535@gmail.com>
2026-01-07 14:23:57 +0300

    Hardcode API URL in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify('https://vesta-backend-08db.onrender.com')
+  }
 })
